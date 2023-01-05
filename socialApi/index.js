8d0d266ca2757e06edb1d3f2d962907f00ba3a17
leerivera <leerivera80@gmail.com>
2023-01-05 15:42:58 -0500

    index

--- socialApi/index.js
+++ socialApi/index.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('Connected to MongoDB');
+});
+
+
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the home page");
+});
 
 
 app.listen(3000, () => {
-  console.log('Server is running dumb fast yo!!!');
+  console.log('The backend is running dumb fast yo!!!');
 })
